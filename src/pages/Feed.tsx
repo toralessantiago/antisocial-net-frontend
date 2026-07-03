e751d8fb8a85c8fef0cf195ae62b0ff1ba2c972d
toralessantiago <torales.santiagoroberto@estudiantes.unahur.edu.ar>
2026-07-03 09:22:06 -0300

    commit

--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -4,7 +4,7 @@ import { PostList } from "../components/PostList";
 function Feed() {
   return (
     <Container className="mt-4">
-      <h2>Muro de publicaciones</h2>
+  
       <PostList />
     </Container>
   );
